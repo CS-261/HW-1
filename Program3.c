48d9b843ea04068f35675c4460b125b73971a1e1
Austin Chayka <32969809+AustinChayka@users.noreply.github.com>
2020-01-09 13:44:25 -0800

    Progress
    Updated code

--- Program3.c
+++ Program3.c
@@ -1,27 +1,33 @@
 /* CS261- HW1 - Program3.c*/
-/* Name:
- * Date:
+/* Name: Austin Chayka
+ * Date: 1.9.2020
  * Solution description:
  */
  
 #include <stdio.h>
 
 void sort(int* number, int n){
-     /*Sort the array of integeres of length n*/     
+     /*Sort the array of integeres of length n*/
+    int i;
+    for(i = 0; i < n - 1; i++) {
+
+    }     
 }
 
 int main(){
     /*Declare an integer n and assign it a value of 20.*/
-    
+    int n = 20;
     /*Allocate memory for an array of n integers using malloc.*/
-    
+    int * arr = (int *)malloc(n * sizeof(int));
     /*Fill this array with random numbers, using rand().*/
-    
+    int i;
+    for(i = 0; i < n; i++) arr[i] = rand();
     /*Print the contents of the array.*/
-
+    for(i = 0; i < n; i++) printf("%i ", arr[i]);
+    printf("\n");
     /*Pass this array along with n to the sort() function of part a.*/
     
-    /*Print the contents of the array.*/    
-    
+    /*Print the contents of the array.*/ 
+
     return 0;
 }
